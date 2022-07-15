6e75578fc03b33ca7d9581230368a7312cb1ed3b
AlexanderVinarsky <vinarskydichtv@gmail.com>
2022-07-15 23:41:11 +0500

    Added argv check

--- Study_RaggedArrays/Source.cpp
+++ Study_RaggedArrays/Source.cpp
@@ -2,9 +2,23 @@
 
 int main(int argc, char ** args)
 {
-	printf("Argc - %i\n", argc);
 	for (int i = 0; i < argc; i++)
-		printf("Args[%i] = %s\n", i, args[i]);
+	{
+		if (strcmp(args[i], "start") == 0)
+		{
+			printf("Lets start!!!\n");
+			system("pause");
+			printf("Start!");
+		}
+		if (strcmp(args[i], "whereami") == 0)
+		{
+			printf("You are here: %s", args[0]);
+			system("pause");
+			exit(0);
+		}
+	}
+
+	printf("Wrong command.\n");
 
 	system("pause");
 	return 0;
